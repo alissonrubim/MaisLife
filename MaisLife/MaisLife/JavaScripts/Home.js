cec31fd5e2d28a3a2399d700df5577003e874773
Mike <maycontx1@gmail.com>
2016-04-28 11:45:24 -0300

    layout  95%

--- MaisLife/MaisLife/JavaScripts/Home.js
+++ MaisLife/MaisLife/JavaScripts/Home.js
@@ -1,21 +1,33 @@
-﻿var benefits = {
-    more: function (li) {
+﻿// BENEFITS VAR
+var benefits = {
+    more: function (span) {
         $("li[data-reactid='more-benefits']").each(function () {
             $(this).removeClass("more");
         });
-        li.attr("data-bool", "true");
+        span.attr("data-bool", "true");
+        span.text("Ver menos");
+
+        var glyphicon = $("span[data-target='more-benefits']");
+        glyphicon.removeClass("glyphicon-chevron-down");
+        glyphicon.addClass("glyphicon-chevron-up");
+
 
     },
-    less: function (li) {
+    less: function (span) {
         $("li[data-reactid='more-benefits']").each(function () {
             $(this).addClass("more");
         });
-        li.attr("data-bool", "false");
-        li.find("span").text("fds");
+        span.attr("data-bool", "false");
+        span.text("Mais benefícios");
+
+        var glyphicon = $("span[data-target='more-benefits']");
+        glyphicon.removeClass("glyphicon-chevron-up");
+        glyphicon.addClass("glyphicon-chevron-down");
     }
 };
 
-$(document).on("click", "li[data-id='more-benefits']", function () {  
+// BENEFITS TRIGGER
+$(document).on("click", "span[data-id='more-benefits']", function () {
     var bool = $(this).attr("data-bool");
     if (bool == "false")
         benefits.more($(this));
