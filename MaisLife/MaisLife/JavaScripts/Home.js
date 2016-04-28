8b478f963a70c474cce3bb50323dcf9647ffc0f7
Mike <maycontx1@gmail.com>
2016-04-28 10:29:38 -0300

    layout 90%

---
+++ MaisLife/MaisLife/JavaScripts/Home.js
@@ -0,0 +1,24 @@
+﻿var benefits = {
+    more: function (li) {
+        $("li[data-reactid='more-benefits']").each(function () {
+            $(this).removeClass("more");
+        });
+        li.attr("data-bool", "true");
+
+    },
+    less: function (li) {
+        $("li[data-reactid='more-benefits']").each(function () {
+            $(this).addClass("more");
+        });
+        li.attr("data-bool", "false");
+        li.find("span").text("fds");
+    }
+};
+
+$(document).on("click", "li[data-id='more-benefits']", function () {  
+    var bool = $(this).attr("data-bool");
+    if (bool == "false")
+        benefits.more($(this));
+    else
+        benefits.less($(this));
+});
