ed1c6a3884247de08cf296b91607899a9b211256
Tiago Sales <tihhjf@hotmail.com>
2016-04-27 23:56:37 -0300

    Login, Logout, CreateCookie

--- MaisLife/MaisLife/Controllers/HomeController.cs
+++ MaisLife/MaisLife/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using MaisLifeModel;
+﻿using MaisLife.Helper;
+using MaisLifeModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,5 +20,49 @@ namespace MaisLife.Controllers
 
             return View();
         }
+
+        //--------------------------------------------------------------------
+
+        public Usuario Login(Usuario user)
+        {
+            if (user != null)
+            {
+                Session["user"] = user;
+            }
+            return user;
+        }
+
+        public Usuario CreateCookie(string email, string senha, Boolean keep)
+        {
+            Usuario user = ConfigDB.Model.Usuarios.Where(f => f.Email == email && f.Senha == senha).First();
+            if (user != null)
+            {
+
+                HttpCookie cookie = new HttpCookie("UserMaisLife");
+                cookie.Value = user.Email;
+
+                //Tempo expiração
+                TimeSpan expiration;
+
+                if (keep)
+                    expiration = new TimeSpan(365, 0, 0, 0);
+                else
+                    expiration = new TimeSpan(0, 20, 0);
+
+
+                cookie.Expires = DateTime.Now + expiration;
+                Response.Cookies.Add(cookie);
+
+            }
+
+            return Login(user);
+        }
+
+        public void Logout()
+        {
+            Session.Remove("User");
+            Request.Cookies.Remove("UserMaisLife");
+        }
+
     }
 }
