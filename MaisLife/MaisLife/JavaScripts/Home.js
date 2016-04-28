f3da8f7def8130c87f93cc249fc6d9afb0529f3a
Mike <maycontx1@gmail.com>
2016-04-28 17:05:28 -0300

    Mudança no layout

--- MaisLife/MaisLife/JavaScripts/Home.js
+++ MaisLife/MaisLife/JavaScripts/Home.js
@@ -1,5 +1,26 @@
-﻿// BENEFITS VAR
+﻿// LINK SCROLL
+$(document).on("click", "a[data-content='scroll-link']", function () {    
+    var id = $(this).attr("data-target");
+    var target = $("#" + id);
+
+    $('html, body').stop().animate({
+        'scrollTop': target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+// BENEFITS VAR
 var benefits = {
+    scroll: function(){
+        var target = $("#benefits");
+
+        $('html, body').stop().animate({
+            'scrollTop': target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    },
     more: function (span) {
         $("li[data-reactid='more-benefits']").each(function () {
             $(this).removeClass("more");
@@ -11,7 +32,7 @@ var benefits = {
         glyphicon.removeClass("glyphicon-chevron-down");
         glyphicon.addClass("glyphicon-chevron-up");
 
-
+        benefits.scroll();
     },
     less: function (span) {
         $("li[data-reactid='more-benefits']").each(function () {
@@ -23,6 +44,8 @@ var benefits = {
         var glyphicon = $("span[data-target='more-benefits']");
         glyphicon.removeClass("glyphicon-chevron-up");
         glyphicon.addClass("glyphicon-chevron-down");
+
+        benefits.scroll();
     }
 };
 
