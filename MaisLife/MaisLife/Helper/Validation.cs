9952a9d46ac96fc49dfe9af7c1abbe8768a48a36
Tiago Sales <tihhjf@hotmail.com>
2016-04-29 23:27:00 -0300

    Classe Validation

---
+++ MaisLife/MaisLife/Helper/Validation.cs
@@ -0,0 +1,32 @@
+﻿using MaisLife.Models.Adapter;
+using MaisLifeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaisLife.Helper
+{
+    public class Validation
+    {
+        public static bool ValidationLogin(string email, string senha)
+        {
+            var user = ConfigDB.Model.Usuarios.Where(f => f.Email == email && f.Senha == senha).FirstOrDefault();
+
+            if (user != null)
+                return true;
+
+            return false;
+        }
+
+        public static bool ExistingEmailValidation(string email)
+        {
+            var user = ConfigDB.Model.Usuarios.Where(f => f.Email == email).FirstOrDefault();
+
+            if (user == null)
+                return true;
+
+            return false;
+        }
+    }
+}
