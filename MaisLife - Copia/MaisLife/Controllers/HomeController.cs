eb1c373ec14e93df5355262f91669bb8cfca3d21
Tiago Sales <tihhjf@hotmail.com>
2016-04-30 11:39:35 -0300

    sync

---
+++ MaisLife - Copia/MaisLife/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using MaisLife.Helper;
+using MaisLife.Models.Adapter;
+using MaisLifeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaisLife.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateContact(ContatoAdapter contato)
+        {
+            ConfigDB.Model.Add(contato.ToContato());
+            if (ConfigDB.Model.HasChanges)
+            {
+                ConfigDB.Model.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CreateUser(UsuarioAdapter user)
+        {
+            if (Validation.ExistingEmailValidation(user))
+            {
+                ConfigDB.Model.Add(user.ToUsuario());
+                if (ConfigDB.Model.HasChanges)
+                {
+                    ConfigDB.Model.SaveChanges();
+                    Sessions.CreateCookie(user.ToUsuario(), false);
+                }
+            }
+            else
+            {
+                TempData["MessageErroRegister"] = "E-mail ja cadastrado";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult LoginUser(UsuarioAdapter user)
+        {
+            if(Validation.ValidationLogin(user))
+            {
+                Sessions.CreateCookie(user.ToUsuario(), false);
+            }
+            else
+            {
+                TempData["MessageErroLogin"] = "E-mail ou senha incorretos";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult LogoutUser()
+        {
+            Sessions.Logout();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
