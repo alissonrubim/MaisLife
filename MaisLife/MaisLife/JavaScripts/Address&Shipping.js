d562bcdfc396beae18318b053d54ddf2346da1ea
Mike <maycontx1@gmail.com>
2016-05-02 23:12:43 -0300

    login arrumado

---
+++ MaisLife/MaisLife/JavaScripts/Address&Shipping.js
@@ -0,0 +1,18 @@
+﻿$(document).on("click", "div[data-content='address']", function () {
+    address.update($(this));
+});
+
+address = {
+    update: function (div) {
+        var tax = div.find("span[data-id='address-tax']");
+        var shipping = $("input[data-id='address-shipping']");
+        var total = $("input[data-id='address-total']");
+
+        shipping.val("R$ " + tax.text());
+        var taxValue = parseFloat(tax.text());        
+        var totalValue = parseFloat(total.val().split(" ")[1]);       
+        total.val(totalValue + taxValue);
+        total.val("R$ " + total.val());
+
+    }
+}
